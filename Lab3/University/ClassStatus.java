3a7121f1cadaea7452ad7851c453704070e4bdcb
Nay Zin Min Lwin <64629282+nayzinminlwin@users.noreply.github.com>
2025-04-15 13:16:05 +0800

    Update ClassStatus.java

--- Lab3/University/ClassStatus.java
+++ Lab3/University/ClassStatus.java
@@ -1,1 +1,15 @@
+package Lab3.University;
 
+public class ClassStatus{
+    private String classStatus;
+
+    ClassStatus(String classStatus)
+    {
+        this.classStatus = classStatus;
+    }
+
+    public String getClassStatus()
+    {
+        return this.classStatus;
+    }
+}
