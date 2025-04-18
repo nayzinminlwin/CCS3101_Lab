641a449a30fba83be73a58f2cfd2d8380972e49f
Nay Zin Min Lwin <nayzinminlwin22@gmail.com>
2025-04-18 20:31:51 +0800

    Added static counter for instance creation.

--- Test1_Prac/Ride.java
+++ Test1_Prac/Ride.java
@@ -1,23 +1,26 @@
 package Test1_Prac;
 
 public class Ride{
-    private int rideID;
+    private static int rideCounter = 0;
+    private String rideID;
     private Driver d0;
     private Passenger p0;
     private double distance;
     private double fare;
 
-    Ride(int rideID, Driver d0, Passenger p0, double distance)
+    Ride(Driver d0, Passenger p0, double distance)
     {
-        this.rideID = rideID;
+        this.rideID = "Ride"+(rideCounter+1);
         this.d0 = d0;
         this.p0 = p0;
         this.distance = distance;
         // System.out.println("Current distance is : "+ this.distance);
         this.fare = distance *10.0;
+
+        rideCounter++;
     }
 
-    public int getRideID()
+    public String getRideID()
     {
         return this.rideID;
     }
@@ -30,6 +33,11 @@ public class Ride{
     public String getFare()
     {
         return String.format("%.2fm cost %.2fRM.\n", this.distance, this.fare);
+    }
+
+    public static int getRideCount()
+    {
+        return rideCounter;
     }
 
 }
--- Test1_Prac/mainRides.java
+++ Test1_Prac/mainRides.java
@@ -9,13 +9,16 @@ public class mainRides{
         Passenger p1 = new Passenger("Cik", 9999);
         Passenger p2 = new Passenger("Siapa", 898989);
 
-        Ride r1 = new Ride(0, d2, p2, 333.00);
-        Ride r2 = new Ride(1, d1, p1, 30.00);
-        Ride r3 = new Ride(2, d2, p1, 10.00);
-        Ride r4 = new Ride(3, d2, p1, 10.00);
+        Ride r1 = new Ride(d2, p2, 333.00);
+        Ride r2 = new Ride(d1, p1, 30.00);
+        Ride r3 = new Ride(d2, p1, 10.00);
+        Ride r4 = new Ride(d1, p2, 0.01);
 
-        Ride[] rides = {r1,r2,r3,null,null};
-        rides[4]=r4;
+        Ride[] rides = new Ride[Ride.getRideCount()];
+        rides[0] = r1;
+        rides[1] = r2;
+        rides[2] = r3;
+        rides[3] = r4;
         int rideCounts = rides.length;
 
         for(int i=0;i<rideCounts;i++)
@@ -23,6 +26,8 @@ public class mainRides{
             if(rides[i]==null) continue;
             System.out.printf("Ride ID : %s \nDriver : %s \nFare : %s\n", rides[i].getRideID(),rides[i].getDriver().getName(),rides[i].getFare());
         }
+
+        System.out.println("Total rides created is : "+ Ride.getRideCount());
         
     }
 }
