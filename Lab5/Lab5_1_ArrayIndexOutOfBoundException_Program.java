4b6bb04da8d75b733b07a4996601b8f5928ed115
Nay Zin Min Lwin <nayzinminlwin22@gmail.com>
2025-05-15 14:00:49 +0800

    Made some changes.

--- Lab5/Lab5_1_ArrayIndexOutOfBoundException_Program.java
+++ Lab5/Lab5_1_ArrayIndexOutOfBoundException_Program.java
@@ -1,36 +1,33 @@
 import java.util.InputMismatchException;
-// import java.util.Random;
 import java.util.Scanner;
 
-public class Lab5_1_ArrayIndexOutOfBoundException_Program
-{
-    public static void main(String[] args)
-    {
+public class Lab5_1_ArrayIndexOutOfBoundException_Program {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] months = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
-        int[] dayCounts = {31,28,31,30,31,30,31,31,30,31,30,31};
+        String[] months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+        int[] dayCounts = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-        try{
+        try {
             System.out.print("Enter number from 1 to 12 : ");
             int userIndex = sc.nextInt();
 
-            System.out.printf("Your month is %s and it has %d days.",months[userIndex-1],dayCounts[userIndex-1]);
-            
-            // while(true){
-            //     int userIndex = 1+(int)(Math.random() * 13);
-            //     System.out.println("Random number : "+ userIndex);
-    
-            //     System.out.printf("Your month is %s and it has %d days.",months[userIndex-1],dayCounts[userIndex-1]);
+            System.out.printf("Your month is %s and it has %d days.", months[userIndex -
+                    1], dayCounts[userIndex - 1]);
+
+            // while (true) {
+            // int userIndex = 1 + (int) (Math.random() * 13);
+            // System.out.println("\nRandom number : " + userIndex);
+
+            // System.out.printf("Your month is %s and it has %d days.", months[userIndex -
+            // 1],
+            // dayCounts[userIndex - 1]);
             // }
-            
-        }
-        catch(InputMismatchException inpEx){
+
+        } catch (InputMismatchException inpEx) {
             System.out.println("Please enter only numbers.");
-        }
-        catch(ArrayIndexOutOfBoundsException arrEx){
+        } catch (ArrayIndexOutOfBoundsException arrEx) {
             System.out.println("You must enter only from 1 to 12.");
-        }
-        finally{
+        } finally {
             sc.close();
         }
     }
