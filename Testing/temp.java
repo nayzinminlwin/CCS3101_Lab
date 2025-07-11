3913205868d4914830da6c5b49732fb090e62f2f
Nay Zin Min Lwin <nayzinminlwin22@gmail.com>
2025-07-11 20:04:24 +0800

    Today the exam is finished. And i think i nailed it. Time to Close  #24, #25, #26, #27, #28.

--- Testing/temp.java
+++ Testing/temp.java
@@ -1,11 +1,18 @@
 package Testing;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class temp {
 
     public static void main(String[] args) {
+        // testing();
+        anotherTesting();
+    }
 
+    public static void testing() {
         int[] temps = { 41, 44, 42, 40, 38, 43, 41, 40, 42, 45, 47, 48 };
         Scanner sc = new Scanner(System.in);
 
@@ -25,4 +32,14 @@ public class temp {
         System.out.printf("Average temperature from %d to %d is %.2f", startHour, endHour, avgTemp);
     }
 
+    public static void anotherTesting() {
+        Calendar myCalendar = new GregorianCalendar(2000, 1, 19);
+        String[] days = { "M", "T", "W", "T", "F", "Sa", "Sunday" };
+
+        int dayIndex = myCalendar.DAY_OF_WEEK - 1;
+        System.out.println("11.10.1654 is " + days[dayIndex]);
+
+        // System.out.println(Calendar.HOUR + ":" + Calendar.MINUTE);
+        // System.out.println(new Date());
+    }
 }
